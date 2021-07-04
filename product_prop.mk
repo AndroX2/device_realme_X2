da131ee43ae1a0769c92b23e276d3b40461b4a6f
HarshAnand-799 <harshanand7898@gmail.com>
2021-07-04 15:56:58 +0000

    X2:Prop:Set DPI
    • Due to some weird Reasons the default DPI Doesn't get set via product_prop.mk . So, We need to define in system.prop too.
    
    Signed-off-by: HarshAnand-799 <harshanand7898@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -19,7 +19,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 
 # Override Display Density
 PRODUCT_PRODUCT_PROPERTIES += \
-    ro.sf.lcd_density=440
+    ro.sf.lcd_density=480
 
 # Use Vulkan backend for Skia UI rendering
 PRODUCT_PRODUCT_PROPERTIES += \
