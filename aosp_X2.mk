6e7a47512f7c76d634e2c7f1cb38adfda63d14b7
AndroX2 <chayan.eee.bsmrstu.bd@gmail.com>
2021-08-10 12:30:06 +0600

    X2: Update Redfin FP to August

--- aosp_X2.mk
+++ aosp_X2.mk
@@ -25,6 +25,6 @@ PRODUCT_MODEL := realme X2
 PRODUCT_MANUFACTURER := realme
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys"
+BUILD_FINGERPRINT := "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
