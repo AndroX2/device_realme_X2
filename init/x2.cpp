7aa4bcf982c259e538133b015e0a18ffab272c38
AndroX2 <chayan.eee.bsmrstu.bd@gmail.com>
2021-08-10 12:30:06 +0600

    X2: Update Redfin FP to August

--- init/x2.cpp
+++ init/x2.cpp
@@ -90,24 +90,24 @@ void setRMX(const unsigned int variant)
 
     //RMX1991
     prop[0] = {
-         "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-        "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+        "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys",
+        "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys",
         "RMX1991",
         "RMX1991CN",
     };
 
     //RMX1992
     prop[1] = {
-         "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-        "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+        "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys",
+        "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys",
         "RMX1992",
         "RMX1992L1",
     };
 
     //RMX1993
     prop[2] = {
-        "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys",
-        "redfin-user 11 RQ3A.210705.001 7380771 release-keys",
+        "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys",
+        "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys",
         "RMX1993",
         "RMX1993L1",
     };
