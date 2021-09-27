be542882c81233327b92b6df526301df5032f769
Retypolkg <retypolkg2021@gmail.com>
2021-09-27 16:15:41 +0200

    X2: add Google Hotword stuff * This fixes OK Google detection
    Signed-off-by: Retypolkg <retypolkg2021@gmail.com>

--- device.mk
+++ device.mk
@@ -121,9 +121,13 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
     frameworks/native/data/etc/android.hardware.telephony.ims.xml:system/etc/permissions/android.hardware.telephony.ims.xml
 
+# Get Hotword stuff
+$(call inherit-product-if-exists, vendor/realme/google/google.mk)
+
 # HotwordEnrollement app permissions
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:system/etc/permissions/privapp-permissions-hotword.xml
+    $(LOCAL_PATH)/configs/privapp-permissions-hotword.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-hotword.xml \
+    $(LOCAL_PATH)/configs/hotword-hiddenapi-package-whitelist.xml:$(TARGET_COPY_OUT_SYSTEM)/etc/sysconfig/hotword-hiddenapi-package-whitelist.xml
 
 # TEMP-HOSTPAD
 PRODUCT_COPY_FILES += \
