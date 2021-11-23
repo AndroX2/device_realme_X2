6e4e5565211594dc2df9d0b41697bb3b64f172cb
ThatisJigen <gianfranco.liguori.96@gmail.com>
2021-11-23 10:03:22 +0600

    X2: fingerprint: report fingerprint pressed on our special node
    * https://github.com/AndroX2/kernel_realme_sm6150/commit/46c9ba355f3398fc63563f4501898b02422d2aeb created a node to track fingerprint press status, trigger it to 1 when pressed, later a service will be added to track file modifications and pulse doze when pressed
    * That way we are going to track the pressed status and via userspace trigger a doze_pulse to handle AoD
    * Send also a fake proximity event that will make proximity start when in aod, that way userspace will handle the doze pulse being triggered by the proximity state change

--- udfps_wrapper/BiometricsFingerprint.cpp
+++ udfps_wrapper/BiometricsFingerprint.cpp
@@ -26,6 +26,9 @@
 
 #define FOD_STATUS_PATH "/sys/kernel/oppo_display/notify_fppress"
 #define DIMLAYER_PATH "/sys/kernel/oppo_display/dimlayer_hbm"
+#define PS_MASK "/proc/touchpanel/prox_mask"
+#define AOD_PRESS "/proc/touchpanel/fod_aod_pressed"
+#define DOZING "/proc/touchpanel/DOZE_STATUS"
 #define STATUS_ON 1
 #define STATUS_OFF 0
 #define BIND(fn) [this](auto&&... args) -> decltype(auto) { return this->fn(std::forward<decltype(args)>(args)...); }
@@ -41,6 +44,15 @@ template <typename T>
 static inline void set(const std::string& path, const T& value) {
     std::ofstream file(path);
     file << value;
+}
+
+template <typename T>
+static inline T get(const std::string& path, const T& def) {
+    std::ifstream file(path);
+    T result;
+
+    file >> result;
+    return file.fail() ? def : result;
 }
 
 BiometricsFingerprint::BiometricsFingerprint() : isEnrolling(false) {
@@ -107,6 +119,10 @@ public:
     }
 
     Return<void> onTouchDown(uint64_t deviceId) {
+        if (get(DOZING, STATUS_OFF)){
+            set(AOD_PRESS, STATUS_ON);
+            set(PS_MASK, STATUS_ON);
+        }
         set(FOD_STATUS_PATH, STATUS_OFF);
         return Void();
     }
