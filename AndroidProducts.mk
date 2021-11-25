83a20f884f4983733360dc1f24d93b3af2ca8868
Chayan <chayan.eee.bsmrstu.bd@gmail.com>
2021-11-25 19:58:57 +0600

    X2: aospa bringup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,10 +15,10 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_X2.mk
+    $(LOCAL_DIR)/aospa_X2.mk
 
 
 COMMON_LUNCH_CHOICES := \
-    aosp_X2-eng \
-    aosp_X2-user \
-    aosp_X2-userdebug
+    aospa_X2-eng \
+    aospa_X2-user \
+    aospa_X2-userdebug
--- aospa_X2.mk
+++ aospa_X2.mk
@@ -6,13 +6,17 @@
 
 $(call inherit-product, device/realme/X2/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from the AOSPA configuration.
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := aosp_X2
+PRODUCT_NAME := aospa_X2
 PRODUCT_DEVICE := X2
 PRODUCT_BRAND := realme
 PRODUCT_MODEL := realme X2
@@ -22,5 +26,5 @@ BUILD_FINGERPRINT := "google/redfin/redfin:12/SPB5.210812.002/7671067:user/relea
 
 PRODUCT_GMS_CLIENTID_BASE := android-oppo
 
-PEX_BUILD_TYPE=OFFICIAL
-PEX _MAINTAINER=Chayan_H
+# Platform
+TARGET_BOARD_PLATFORM := sm6150
